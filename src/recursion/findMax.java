8ff1223308159519ccd2fe06218c0f300cc6f2b0
joshu2761 <joshua.y0761@gmail.com>
2024-11-25 17:52:48 -0500

    Completed FindMax

---
+++ src/recursion/findMax.java
@@ -0,0 +1,22 @@
+package recursion;
+
+public class findMax {
+    
+    /**
+     * Find the maximum element in an array
+     * @param arr
+     * @param n
+     * @return maximum element
+     */
+    public static int findMax(int[] arr, int n) {
+        if (n == 1) {
+            return arr[0];
+        }
+        return Math.max(arr[n - 1], findMax(arr, n - 1));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        System.out.println(findMax(arr, arr.length)); // 5
+    }
+}
