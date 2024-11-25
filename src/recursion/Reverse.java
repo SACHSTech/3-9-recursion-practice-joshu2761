933bb6035a884b6aa117b534a6e84cd4000c6398
joshu2761 <joshua.y0761@gmail.com>
2024-11-25 10:50:56 -0500

    Completed problem 4

---
+++ src/recursion/Reverse.java
@@ -0,0 +1,23 @@
+package recursion;
+/*
+ * 4. Reverse a String
+Write a recursive function reverse(String s) that takes a string s and returns it reversed.
+ */
+public class Reverse {
+    
+    /**
+     * Reverse a string
+     * @param s
+     * @return reversed string
+     */
+    public static String reverse(String s) {
+        if (s.isEmpty()) {
+            return s;
+        }
+        return reverse(s.substring(1)) + s.charAt(0);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse("hello")); // Output: olleh
+    }
+}
